4cb909af723ad10152748ead25fd7d4b2442bf56
dnozaki <dnozaki88888888@gmail.com>
2023-08-10 17:03:45 +0900

    makeApi u016-u017

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const CryptoJS = require('crypto-js');
+const User = require('./src/v1/models/user');
 const app = express();
 const PORT = 5000;
 require('dotenv').config();
@@ -12,6 +14,18 @@ try {
   console.log(err);
 }
 //ユーザー新規登録API
+app.post('/register', async (req, res) => {
+  //パスワードの受け取り
+  const password = req.body.password;
+  try {
+    //パスワードの暗号化
+    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+    //ユーザーの新規作成
+    const user = await User.create(req.body);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 //ユーザーログイン用API
 
