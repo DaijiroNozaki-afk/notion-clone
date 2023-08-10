076c1b552724d019c98e335f707c2f757cd9dbce
dnozaki <dnozaki88888888@gmail.com>
2023-08-10 16:08:57 +0900

    connect server2db u013

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Express');
-});
+//DB接続
+try {
+  mongoose.connect(
+    'mongodb+srv://dnozaki88888888:TQY9iu1V5qIHKjkT@cluster0.xjffgte.mongodb.net/?retryWrites=true&w=majority'
+  );
+  console.log('DB と接続中');
+} catch (err) {
+  console.log(err);
+}
+//ユーザー新規登録API
+
+//ユーザーログイン用API
 
 app.listen(PORT, () => {
   console.log('ローカルサーバー起動中・・・');
