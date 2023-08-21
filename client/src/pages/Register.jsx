e8893db6fa5ebb4ec04c5b23d33d1eee6942a20d
dnozaki <dnozaki88888888@gmail.com>
2023-08-21 16:59:26 +0900

    userRegister u050

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,6 +2,8 @@ import { Box, Button, TextField } from '@mui/material';
 import React from 'react';
 import { LoadingButton } from '@mui/lab';
 import { Link } from 'react-router-dom';
+import authApi from '../api/authApi';
+
 const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -13,6 +15,19 @@ const Register = () => {
     console.log(username);
     console.log(password);
     console.log(confirmPassword);
+
+    //新規登録APIを叩く
+    try {
+      const res = await authApi.register({
+        username,
+        password,
+        confirmPassword,
+      });
+      localStorage.setItem('token', res.token);
+      console.log('新規登録に成功しました。');
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <>
