d1edd8add92e8eeb59698af7b84150c487afe51a
dnozaki <dnozaki88888888@gmail.com>
2023-08-24 16:53:01 +0900

    make sidebar u066-u067

--- client/src/components/layout/common/Sidebar.jsx
+++ client/src/components/layout/common/Sidebar.jsx
@@ -1,7 +1,77 @@
+import {
+  Box,
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  Typography,
+} from '@mui/material';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
 import React from 'react';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <Drawer
+      container={window.document.body}
+      variant="permanent"
+      open={true}
+      sx={{ width: 250, height: '100vh' }}
+    >
+      <List sx={{ width: 250, height: '100vh' }}>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              shincode
+            </Typography>
+            <IconButton>
+              <LogoutOutlinedIcon />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+        <Box sx={{ paddingTop: '10px' }}></Box>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              お気に入り
+            </Typography>
+          </Box>
+        </ListItemButton>
+        <Box sx={{ paddingTop: '10px' }}></Box>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              プライベート
+            </Typography>
+            <IconButton>
+              <AddBoxOutlinedIcon fontSize="small" />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+      </List>
+    </Drawer>
+  );
 };
 
 export default Sidebar;
