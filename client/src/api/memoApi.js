3f25ff52e1d307d704463fbcc1bdeaa45f78741c
dnozaki <dnozaki88888888@gmail.com>
2023-08-25 16:40:33 +0900

    getOneMemo u090

--- client/src/api/memoApi.js
+++ client/src/api/memoApi.js
@@ -3,6 +3,7 @@ import axiosClient from './axiosClient';
 const memoApi = {
   create: (params) => axiosClient.post('memo'),
   getAll: (params) => axiosClient.get('memo'),
+  getOne: (id) => axiosClient.get(`memo/${id}`),
 };
 
 export default memoApi;
