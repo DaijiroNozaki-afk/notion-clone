b1096f3d39119393b23212976dcdf02ce7a18552
dnozaki <dnozaki88888888@gmail.com>
2023-09-04 18:56:25 +0900

    dnd test with dummyItem 20230904

--- client/src/components/layout/common/Sidebar.jsx
+++ client/src/components/layout/common/Sidebar.jsx
@@ -14,6 +14,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import memoApi from '../../../api/memoApi';
 import { setMemo } from '../../../redux/features/memoSlice';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 const Sidebar = () => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -27,7 +28,29 @@ const Sidebar = () => {
     localStorage.removeItem('token');
     navigate('/login');
   };
-
+  // テスト用
+  const items = [
+    {
+      id: 'item-0',
+      content: 'item 0',
+    },
+    {
+      id: 'item-1',
+      content: 'item 1',
+    },
+    {
+      id: 'item-2',
+      content: 'item 2',
+    },
+    {
+      id: 'item-3',
+      content: 'item 3',
+    },
+    {
+      id: 'item-4',
+      content: 'item 4',
+    },
+  ];
   useEffect(() => {
     const getMemos = async () => {
       try {
@@ -54,6 +77,25 @@ const Sidebar = () => {
     } catch (err) {
       alert(err);
     }
+  };
+  const grid = 8;
+  const getListStyle = (isDraggingOver) => ({
+    background: isDraggingOver ? 'lightblue' : 'lightgrey',
+    width: '250px',
+    padding: grid,
+  });
+
+  const getItemStyle = (isDragging, draggableStyle) => ({
+    useSelect: 'none',
+    padding: grid * 2,
+    margin: `0 0 ${grid} 0`,
+    background: isDragging ? 'lightgreen' : 'grey',
+    ...draggableStyle,
+  });
+  const [dndState, setDndState] = useState(items);
+  // ドラッグ後に位置が変わっていた場合、順序入れ替えをする
+  const onDragEnd = (result) => {
+    console.log(result);
   };
   return (
     <Drawer
@@ -143,6 +185,56 @@ const Sidebar = () => {
             <Typography>
               {item.icon} {item.title}
             </Typography>
+          </ListItemButton>
+        ))}
+        <ListItemButton>
+          <Box>
+            <Typography variant="body2" fontWeight="700">
+              DND
+            </Typography>
+          </Box>
+        </ListItemButton>
+
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+              >
+                {dndState.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
+                      >
+                        {item.content}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+
+        {memos.map((item, index) => (
+          <ListItemButton
+            sx={{ pl: '20px' }}
+            component={Link}
+            to={`/memo/${item._id}`}
+            key={item._id}
+            selected={index === activeIndex}
+          >
+            <Typography>{/* {item.icon} {item.title} */}</Typography>
           </ListItemButton>
         ))}
       </List>
